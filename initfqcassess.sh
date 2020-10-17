6f6baddcd1c0d24542bf621055bfb8d60a5144b8
B169959-2020 <XXXXXXXXXX>
2020-10-17 22:05:38 +0100

    integrated startfastqc.sh and fqcassess.sh

---
+++ initfqcassess.sh
@@ -0,0 +1,52 @@
+mkdir results
+fastqc --extract -o results /localdisk/data/BPSM/Assignment1/fastq/*.fq.gz
+
+ls ~/assignment1/results|
+while read file_name
+do
+if [[ $file_name == *".html" || $file_name == *".zip" ]]
+then
+continue
+else
+echo $file_name
+fi
+done|sort>~/assignment1/dirlist.txt
+
+echo -e "Wellcome!\n">~/assignment1/assessment.txt #just to start a new file
+
+while read dir_name
+do
+echo -e "\nThis is the result of $dir_name:">>~/assignment1/assessment.txt
+Q=0    #set original parameter for printing
+while read line
+do
+if [[ $line == *"Total Sequences"* ]]
+then
+echo ">>$line"
+fi
+
+if [[ $line == *"Sequences flagged as poor quality"* ]]
+then
+echo ">>$line"
+fi
+
+if [[ $line == *"Per sequence quality scores"* ]]
+then
+echo ">Per sequence quality scores"
+Q=1     #set a parameter to print following lins
+continue
+fi
+
+if [[ $line == ">>END_MODULE" ]]
+then
+Q=0     #stop printing at each end
+fi
+
+if [[ $Q == 1 ]] 
+then
+echo $line
+fi
+
+done<~/assignment1/results/$dir_name/fastqc_data.txt>>~/assignment1/assessment.txt
+
+done<~/assignment1/dirlist.txt
