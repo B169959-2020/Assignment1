68f24a75c5999467c8063c0c9c3d9f7df0bb61cd
B169959-2020 <XXXXXXXXXX>
2020-10-20 19:59:30 +0100

    bam_bed.sh group.sh average.sh added, everything for getting  mean

--- bedgene/bam_bed.sh
+++ bedgene/bam_bed.sh
@@ -1,10 +1,28 @@
+mkdir calculatedir
 cp /localdisk/data/BPSM/Assignment1/Tbbgenes.bed ./
 IFS=$'\t'
-bedtools intersect -a Tbbgenes.bed -b  ../bowtiealign/bowbam/222_L8_1.bam -c|
+
+ls ../bowtiealign/bowbam|sort|
+while read bam
+do
+bedtools intersect -a Tbbgenes.bed -b  ../bowtiealign/bowbam/$bam -c|
 while read c1 c2 c3 name c5 c6 count
 do
 if [[ $c5 == "gene" ]]
 then
 echo -e "$name\t$count" 
 fi
+done > calculatedir/$bam.count
 done
+
+
+
+
+
+
+
+
+
+
+
+
