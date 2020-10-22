656ffe24a8f879bb3da8276960c15adffec4f7a6
B169959-2020 <XXXXXXXXXX>
2020-10-22 16:09:02 +0100

    optimizing the parameters of rankchange.sh

--- Assignment_1/rankchange.sh
+++ Assignment_1/rankchange.sh
@@ -3,18 +3,18 @@ echo -e "#Gene_name\tSlenderTbb_mean\tStumpyTbb_mean\tDifference\tFold\tIndicato
 
 while read name slender stumpy
 do
-#discard the data that is useless
-if [[ $slender < 1 && $stumpy < 1 || $name == "#Gene_name" ]] 
+#set parameter to discard the data that is useless
+if [[ $slender < 5 && $stumpy < 5 || $name == "#Gene_name" ]] 
 then
 continue 
 else
 if [[ $slender == 0 ]] 
 then
-slender=0.01 #denominator below cannot be 0
+slender=0.0001 #denominator below cannot be 0
 fi
 if [[ $stumpy == 0 ]]
 then
-stumpy=0.01 #denominator below cannot be 0
+stumpy=0.0001 #denominator below cannot be 0
 fi
 fi
 echo -e "$name\t$slender\t$stumpy"
@@ -27,7 +27,7 @@ if(($2-$3) < 0)
 else if(($2-$3) >= 0)
 {print $1"\t"$2"\t"$3"\t"($2-$3)"\t"($2/$3)"\t""+"}
 }' |
-#rank every gene in terms of difference first and then fold, the top genes are those significantly changed after transformation.
-sort -k4,4nr -k5,5nr >> diff_fold.txt 
+#rank every gene in terms of difference and fold, the top 100 genes are those significantly changed after transformation.
+sort -k4nr|head -1000|sort -k5nr|head -100 >> diff_fold.txt 
 
 echo "Done, see result in diff_fold.txt"
