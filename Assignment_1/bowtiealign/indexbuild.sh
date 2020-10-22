0de32c8ead2284e959b686f4bd9efbd1f3642dc2
B169959-2020 <XXXXXXXXXX>
2020-10-22 16:28:14 +0100

    Assignment1.zip

--- Assignment_1/bowtiealign/indexbuild.sh
+++
@@ -1,5 +0,0 @@
-mkdir Tbbgenomeindex #make a dir for Tbbgeno database
-cp /localdisk/data/BPSM/Assignment1/Tbb_genome/Tb927_genome.fasta.gz ./
-gunzip Tb927_genome.fasta.gz 
-#build a Tbbgenome database
-bowtie2-build Tb927_genome.fasta Tbbgenomeindex/tbbgenome 
